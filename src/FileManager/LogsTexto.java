2587972e9954fd5f607879674cc2ae09558f0dfc
halepensis <antgranada@gmail.com>
2025-05-07 08:12:49 +0200

    Add log file reading and writing functionality
    Implemented methods to read logs from a file and write logs to it. Ensured the log file is created if it doesn't exist, and all actions handle potential I/O exceptions gracefully. This enhances log management within the application.

--- src/FileManager/LogsTexto.java
+++ src/FileManager/LogsTexto.java
@@ -1,6 +1,33 @@
 package src.FileManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class LogsTexto {
+    static List<String> logs;
+    static String routePath="src/FileManager/visitas.txt";
+    public static void readLogs() throws IOException {
+            Path path = Paths.get(routePath);
+            if(!Files.exists(path)){
+                Files.createFile(path);
+            }
+            logs = Files.readAllLines(path);
+            for(String linea : logs){
+                System.out.println(linea);
+            }
+    }
+    public static void writeLogs(String log){
+        logs.add(log);
+        try {
+            Files.write(Paths.get(routePath),logs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
