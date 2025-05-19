cd9ce5876f528e0290d669e354f452fe715bb277
halepensis <antgranada@gmail.com>
2025-05-19 09:17:41 +0200

    Refactor LogsTexto formatting for consistency and readability
    Adjusted code formatting to improve readability and maintain consistent style, including proper spacing and indentation. These changes enhance the clarity of the code without modifying its functionality.

--- src/FileManager/LogsTexto.java
+++ src/FileManager/LogsTexto.java
@@ -11,33 +11,34 @@ import java.util.List;
 
 public class LogsTexto {
     static List<String> logs = new ArrayList<>();
-    static String routePath="src/FileManager/visitas.txt";
+    static String routePath = "src/FileManager/visitas.txt";
+
     public static void readLogs() throws IOException {
-            Path path = Paths.get(routePath);
-            if(!Files.exists(path)){
-                Files.createFile(path);
-            }
-            logs = Files.readAllLines(path);
-            for(String linea : logs){
-                System.out.println(linea);
-            }
+        Path path = Paths.get(routePath);
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        logs = Files.readAllLines(path);
+        for (String linea : logs) {
+            System.out.println(linea);
+        }
     }
-    public static void writeLogs(Valoracion valoracion){
+
+    public static void writeLogs(Valoracion valoracion) {
         String nuevoLog = convertirValoracion(valoracion);
         logs.add(nuevoLog);
         try {
-            Files.write(Paths.get(routePath),logs);
+            Files.write(Paths.get(routePath), logs);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String convertirValoracion(Valoracion valoracion){
+    private static String convertirValoracion(Valoracion valoracion) {
         return String.format("%s - VISITA - %s accedió a exposición %s",
 
-                valoracion.getFechaValoracion(),valoracion.getVisitante().getNombre(),valoracion.getExposicion().getTitulo());
+                valoracion.getFechaValoracion(), valoracion.getVisitante().getNombre(), valoracion.getExposicion().getTitulo());
     }
-
 
 
 }
